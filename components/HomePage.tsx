315deb59697d437444eed28ba96cfb843876aa0b
Firman7z <firman@gmail.com>
2025-09-25 13:41:34 +0530

    Mengganti URL gambar profil menjadi tanpa spasi

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -19,7 +19,7 @@ const HomePage: React.FC<HomePageProps> = ({ setActivePage }) => {
     setLightboxImage(null);
   };
 
-  const schoolImageUrl = 'https://raw.githubusercontent.com/Firman7z/SMK-UMAR-FATAH/main/images/GEDUNG2.jpg';
+  const schoolImageUrl = 'https://raw.githubusercontent.com/Firman7z/SMK-UMAR-FATAH/main/images/KI.jpg';
 
   return (
     <div className="animate-fade-in">
